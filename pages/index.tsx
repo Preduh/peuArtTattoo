ab697dcbc9c171f1d994fcf99a9d5abf88f42e52
Preduh <pedro.lucas5860@gmail.com>
2021-06-28 12:32:46 -0300

    Mobile adapted

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,22 @@ import { NextPage } from 'next'
 const Landing: NextPage = () => {
   return (
     <div className={styles.landing}>
-      <header className={styles.header}>
-        <a href="/" className={styles.logo}>
-          Peu Tatto
-        </a>
-        <div className={styles.menu}>
-          <a href="/">Home</a>
-          <a href="#">Contato</a>
-          <a href="portifolio">Portifólio</a>
-        </div>
-      </header>
+      <div className={styles.firstView} id="home">
+        <header className={styles.header}>
+          <img src="/images/logo.png" id={styles.logo} />
+          <div className={styles.menu}>
+            <a href="#home">Home</a>
+            <a href="#contacts">Contato</a>
+            <a href="#portifolio">Portifólio</a>
+          </div>
+        </header>
+      </div>
+      <div className={styles.contacts} id="contacts">
+        Área de Contatos
+      </div>
+      <div className={styles.contacts} id="portifolio">
+        Área do Portifólio
+      </div>
     </div>
   )
 }
